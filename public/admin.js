01ac3d88496f652b71ca38eb92b54c5ab3ee12d8
unknown <jason.wong6507@gmail.com>
2022-11-08 21:22:46 -0500

    Added the basic fetch from the lesson

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,14 @@
 
 // Your Code Here
+function updateBook(bookId){
+fetch("http://localhost:3001/updateBook",{
+	method:"PATCH",
+	Headers:{
+      'Content-Type': 'application/json'
+      },
+body: JSON.stringify({
+    "id": bookId,
+    "title": "Legends of Arathrae",
+})
+})
+}
