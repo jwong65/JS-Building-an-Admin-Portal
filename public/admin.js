b7708edc9bc65b982ea439eb0b25e09f9811f606
unknown <jason.wong6507@gmail.com>
2022-11-10 01:24:19 -0500

    Following the panopto video, using PATCH id 3 is changed and admin.js has the index skeleton

--- public/admin.js
+++ public/admin.js
@@ -1,14 +1,40 @@
 
 // Your Code Here
-function updateBook(bookId){
-fetch("http://localhost:3001/updateBook",{
-	method:"PATCH",
-	Headers:{
-      'Content-Type': 'application/json'
-      },
-body: JSON.stringify({
-    "id": bookId,
-    "title": "Legends of Arathrae",
-})
-})
+
+// function updateBook(bookId){
+// fetch("http://localhost:3001/updateBook",{
+// 	method:"PATCH",
+// 	Headers:{
+//       'Content-Type': 'application/json'
+//       },
+// body: JSON.stringify({
+//     id : bookId,
+//     title : "Legends of Arathrae",
+// })
+// })
+// }
+
+async function main() {
+
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+
+    books.forEach(renderBook)
 }
+
+function renderBook(book) {
+    let bookContainer = document.querySelector('.book-container')
+    bookContainer.innerHTML += `
+        <div class="col-sm-3">
+            <div class="card" style="width: 100%;">
+                <div class="card-body">
+                    <h5 class="card-title">${book.title}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Available: ${book.quantity}</h6>
+                    <p class="card-text">${book.description}</p>
+                </div>
+            </div>
+        </div>
+    `
+}
+
+main()
