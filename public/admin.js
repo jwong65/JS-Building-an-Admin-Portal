5590756caac0308ade2877824504ef51010a4fa1
unknown <jason.wong6507@gmail.com>
2022-11-10 01:48:57 -0500

    Testing the updateBooks function had to make headers lowercase because it wasn't working

--- public/admin.js
+++ public/admin.js
@@ -1,19 +1,6 @@
 
 // Your Code Here
 
-// function updateBook(bookId){
-// fetch("http://localhost:3001/updateBook",{
-// 	method:"PATCH",
-// 	Headers:{
-//       'Content-Type': 'application/json'
-//       },
-// body: JSON.stringify({
-//     id : bookId,
-//     title : "Legends of Arathrae",
-// })
-// })
-// }
-
 async function main() {
 
     let response = await fetch('http://localhost:3001/listBooks')
@@ -21,6 +8,8 @@ async function main() {
 
     books.forEach(renderBook)
 }
+
+
 
 function renderBook(book) {
     let bookContainer = document.querySelector('.book-container')
@@ -30,15 +19,32 @@ function renderBook(book) {
                 <div class="card-body">
                     <h5 class="card-title">${book.title}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">Available: 
-                        <input type="number" value=${book.quantity}> </input>
-                        <button type="button">Submit</button>
+                        <input id="book-quantity-${book.id}" type="number" value=${book.quantity}> </input>
+                        <button type="button" onclick="updateBook(${book.id})">Submit</button>
                     </h6>
-
                     <p class="card-text">${book.description}</p>
                 </div>
             </div>
         </div>
     `
 }
+
+
+
+function updateBook(bookId){
+
+    const bookNumber = document.querySelector(`#book-quantity-${bookId}`)
+
+    fetch("http://localhost:3001/updateBook", {
+        method:"PATCH",
+        headers:{
+          'Content-Type': 'application/json'
+          },
+        body: JSON.stringify({
+        id : bookId,
+        quantity: bookNumber.value
+    })
+    })
+}    
 
 main()
